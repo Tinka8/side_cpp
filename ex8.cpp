9f367b843929a8a3ed98b341b26a1c15cbd7f6ab
Tinka8 <lenmatka818@gmail.com>
2021-12-14 23:29:46 +0100

    array

--- ex8.cpp
+++ ex8.cpp
@@ -2,42 +2,25 @@
 
 int main() {
 	// initiate variables to store total score
-	int gryffindor = 0, slytherin = 0, ravenclaw = 0, hufflepuff = 0;
-	
+	std::string colleges[4] = { "Gryffindor ", "Slytherin ", "Ravenclaw ", "Hufflepuff " };
+
 	// initiate variable to hold round number
-	int i = 1, j;
+	int i = 1, j, k;
 	
 	// initiate variables to store round results
-	int tempGryffindor, tempSlytherin, tempRavenclaw, tempHufflepuff;
+	int scores[4] = {0, 0, 0, 0};;
+	int tempScores[4] = {0, 0, 0, 0};
 
 	// repeat asking for round results until one of the teams achieved atleast 10 score
-	while(gryffindor < 10 && slytherin < 10 && ravenclaw < 10 && hufflepuff < 10) {
+	while(scores[0] < 10 && scores[1] < 10 && scores[2] < 10 && scores[3] < 10) {
 		
 		std::cout << "\nHow much score did teams get in round " + std::to_string(i) + "\n";
-		
-		// ask gryffindor
-		std::cout << "Gryffindor: ";
-		std::cin >> tempGryffindor;
-			
-		gryffindor = gryffindor + tempGryffindor;
-		
-		// ask slytherin
-		std::cout << "Slytherin: ";
-		std::cin >> tempSlytherin;
-			
-		slytherin = slytherin + tempSlytherin;
 
-        // ask ravenclaw
-		std::cout << "Ravenclaw: ";
-		std::cin >> tempRavenclaw;
-			
-		ravenclaw = ravenclaw + tempRavenclaw;
-
-        // ask hufflepuff
-		std::cout << "Hufflepuff: ";
-		std::cin >> tempHufflepuff;
-			
-		hufflepuff = hufflepuff + tempHufflepuff;
+		for (k = 0; k < 4; k++) {
+			std::cout << colleges[k];
+			std::cin >> tempScores[k];
+			scores[k] = scores[k] + tempScores[k];
+		}
 
         // increment round number
         i++;
@@ -45,24 +28,24 @@ int main() {
 
     // print out the total results
     std::cout << "\nSeason ended with following results:";
-    std::cout << "\nGryffindor: "  + std::to_string(gryffindor);
-    std::cout << "\nSlytherin: "  + std::to_string(slytherin);
-    std::cout << "\nRavenclaw: "  + std::to_string(ravenclaw);
-    std::cout << "\nHufflepuff: "  + std::to_string(hufflepuff) << std::endl;
+    std::cout << "\nGryffindor: "  + std::to_string(scores[0]);
+    std::cout << "\nSlytherin: "  + std::to_string(scores[1]);
+    std::cout << "\nRavenclaw: "  + std::to_string(scores[2]);
+    std::cout << "\nHufflepuff: "  + std::to_string(scores[3]) << std::endl;
 
 	std::cout << "\nThere was " << i << " rounds this season" << std::endl;
 
 	for (j = 100; j >= 0; j--) {
-		if (j == gryffindor) {
+		if (j == scores[0]) {
 			std::cout << "\nGryffindor " << j;
 		} 
-		if (j == slytherin) {
+		if (j == scores[1]) {
 			std::cout << "\nSlytherin " << j;
 		} 
-		if (j == ravenclaw) {
+		if (j == scores[2]) {
 			std::cout << "\nRavenclaw " << j;
 		} 
-		if (j == hufflepuff) {
+		if (j == scores[3]) {
 			std::cout << "\nHufflepuff " << j;
 		}
 	}
