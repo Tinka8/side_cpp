6b346d20f66f06dd1ed7e40a47d01456c7a13f74
Tinka8 <lenmatka818@gmail.com>
2021-12-17 21:27:24 +0100

    change variable name

--- ex8.cpp
+++ ex8.cpp
@@ -2,14 +2,14 @@
 
 int main() {
 	// initiate variables to store total score
-	std::string colleges[4] = { "Gryffindor ", "Slytherin ", "Ravenclaw ", "Hufflepuff " };
+	const int arrayLength = 4;
+	std::string colleges[arrayLength] = { "Gryffindor ", "Slytherin ", "Ravenclaw ", "Hufflepuff " };
 
 	// initiate variable to hold round number
-	int i = 1, j, k, m, n;
-	int array = 4;
+	int j, k, m, n, i = 1;
 	
 	// initiate variables to store round results
-	int scores[4] = {0, 0, 0, 0};;
+	int scores[4] = {0, 0, 0, 0};
 	int tempScores[4] = {0, 0, 0, 0};
 
 	// repeat asking for round results until one of the teams achieved atleast 10 score
@@ -17,7 +17,7 @@ int main() {
 		
 		std::cout << "\nHow much score did teams get in round " + std::to_string(i) + "\n";
 
-		for (k = 0; k < array; k++) {
+		for (k = 0; k < arrayLength; k++) {
 			std::cout << colleges[k];
 			std::cin >> tempScores[k];
 			scores[k] = scores[k] + tempScores[k];
@@ -30,17 +30,17 @@ int main() {
     // print out the total results
     std::cout << "\nSeason ended with following results:" << std::endl;
 
-	for (n = 0; n < array; n++) {
-		std::cout << colleges[n] << std::to_string(scores[n]);
+	for (m = 0; m < arrayLength; m++) {
+		std::cout << colleges[m] << std::to_string(scores[m]);
 		std::cout << "\n";
 	}
 
 	std::cout << "\nThere was " << i << " rounds this season" << std::endl;
 
 	for (j = 100; j >= 0; j--) {
-		for (m = 0; m < array; m++)
-		if (j == scores[m]) {
-			std::cout <<  colleges[m] << j;
+		for (n = 0; n < arrayLength; n++)
+		if (j == scores[n]) {
+			std::cout <<  colleges[n] << j;
 			std::cout << "\n";
 		}
 	}
